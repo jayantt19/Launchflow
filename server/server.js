5e40a749cd63039d5336f419ac25266e74843c51
Jayant Kumar Sharma <jayant.sharma_cs22@gla.ac.in>
2026-09-06 23:19:59 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
-const app=require('./src/app');
-const connectDB=require('./src/configs/db')
+
+const app = require("./src/app");
+const connectDB = require("./src/configs/db");
 
 connectDB();
 
-app.listen(3000,(req,res)=>{
-    console.log("Server running on port 3000");
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
