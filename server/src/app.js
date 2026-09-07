4c8b1b5f50fa14fc2968e846dc6cf5f72356a069
Jayant Kumar Sharma <jayant.sharma_cs22@gla.ac.in>
2026-09-07 08:37:20 +0530

    Fix CORS configuration for production

--- server/src/app.js
+++ server/src/app.js
@@ -11,8 +11,7 @@ const knowledgeRoutes = require("./routes/knowledgeRoutes");
 const aiRoutes=require("../src/routes/aiRoutes")
 
 app.use(cors({
-    origin: "http://localhost:5173",
-    "https://launchflow-rypn.onrender.com",
+    origin:"https://launchflow-rypn.onrender.com",
     credentials: true
 }));
 app.use(express.json());
